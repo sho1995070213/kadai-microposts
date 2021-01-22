12750966b93151af6c574f29d9c561c320150ac4
shou.fukuhara <sho.fukuhara0213@gmail.com>
2021-01-22 12:43:45 +0000

    user login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,7 +22,7 @@
                 
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::password('password', ['class' => 'form-control'] !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
                 
                 <div class="form-group">
