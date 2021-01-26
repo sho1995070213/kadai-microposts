884b59c7da5bbc1ebcfc89f4bbeaca1c464af4b5
shou.fukuhara <sho.fukuhara0213@gmail.com>
2021-01-26 02:30:36 +0000

    post

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,7 +14,7 @@
                 {{-- ユーザ登録ページへのリンク --}}
                  <li class="nav-item">{!! link_to_route('users.index','Users', [], ['class' => 'nav-link']) !!}</li>
                  <li class="nav-item dropdown">
-                     <a href="#" class="navlink dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                      <ul class="dropdown-menu dropdown-menu-right">
                          {{-- ユーザ詳細ページへのリンク --}}
                          <li class="dropdown-item">{!! link_to_route('users.show','My profile', ['user' => Auth::id()]) !!}</
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,11 +16,23 @@
         <div class="col-sm-8">
             <ul class="nav-tabs nav-justified mb-3">
                 {{-- ユーザ詳細タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">TimeLine</li>
+                <li class="nav-item">
+                    <a href="{{ route('users.show', ['user' => $user->id }}" class="nav-link {{ Request::routeIs('user.show') ? 'active' : '' }}">
+                        TimeLine
+                        <span class="badge badge-secondary">{{ $user->microposts_count }}</span>
+                    </a>
+                </li>
                 {{-- フォロー一覧タブ --}}
                 <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
                 {{-- フォロワー一覧タブ --}}
                 <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
             </ul>
+            @if (Auth::id() == $user->id)
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
+            @endif
+            {{-- 投稿一覧 --}}
+            @include('microposts.microposts')
         </div>
     </div>
+@endsection
